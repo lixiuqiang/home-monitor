c921a1f69661f7c9f3eb3aa0de0315634b666c89
nibaogang@gmail.com <nibaogang@gmail.com@82a9ad46-b6e1-b26a-e331-0e172ff7a269>
2014-12-06 15:26:33 +0000

    tijiao

--- trunk/home-monitor/java/core/com/handpet/rtsp/INotify.java
+++ trunk/home-monitor/java/core/com/handpet/rtsp/INotify.java
@@ -1,10 +1,17 @@
 package com.handpet.rtsp;
 
 public interface INotify {
+	String CONFIG_WEB = "web";
+	String CONFIG_MONITOR_1 = "monitor_1";
+	String CONFIG_MONITOR_2 = "monitor_2";
+	String CONFIG_TTL_1 = "ttl_1";
+	String CONFIG_TTL_2 = "ttl_2";
 
 	void notify(String title, String text);
 
 	void notify(String title, String text, boolean sendEmail);
+
+	void monitor(String key, String value);
 
 	void shutdown();
 }
