68d7832e44411f0084ab16cf9f39a1b16fe236c8
nibaogang@gmail.com <nibaogang@gmail.com@82a9ad46-b6e1-b26a-e331-0e172ff7a269>
2015-03-04 16:08:02 +0000

    tijiao

---
+++ trunk/home-monitor/java/src/com/nbg/ttl/TTLService2.java
@@ -0,0 +1,14 @@
+package com.nbg.ttl;
+
+public class TTLService2 extends TTLService {
+
+	public TTLService2() {
+		super(4, CONFIG_TTL_2);
+	}
+
+	@Override
+	public int index() {
+		return 1;
+	}
+
+}
